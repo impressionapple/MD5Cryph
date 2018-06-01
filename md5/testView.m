c7047db8c37b1b932b840b7690f3874fde383052
liwenquan <1145821676@qq.com>
2018-06-01 15:21:28 +0800

    init

---
+++ md5/testView.m
@@ -0,0 +1,24 @@
+//
+//  testView.m
+//  testimage
+//
+//  Created by Mac_UI on 2018/6/1.
+//  Copyright © 2018年 Mac_UI. All rights reserved.
+//
+
+#import "testView.h"
+
+@implementation testView
+
+
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+
+-(instancetype)initWithFrame:(CGRect)frame{
+    if (self = [super initWithFrame:frame]) {
+        self = [[NSBundle mainBundle] loadNibNamed:@"testView" owner:self options:nil].lastObject;
+    }
+    return self;
+}
+
+@end
